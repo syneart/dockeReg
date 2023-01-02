c6a5f933a4c6cb25fefb360d6c956163744601e2
Jed Hong <sa@syneart.com>
2023-01-02 23:26:17 +0800

    Update to support Docker Hub

--- gen_script.sh
+++ gen_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# gen_script Shell script by SyneArt <sa@syneart.com> 2023/01/01
-# List docker registry repos and image tags.
+# gen_script shell script by SyneArt <sa@syneart.com> 2023/01/01
+# Get the list of images and tags for a Docker Hub / Docker Registry
 
 _FILE_NAME=list_docker_registry_repos.sh
 
@@ -11,6 +11,8 @@ _REG_AUTH_URL=""
 _REG_URL=""
 
 _USE_OAUTH2=0
+_USE_DOCKER_HUB=0
+_HUB_ORG=""
 
 usage () {
     echo "Usage: gen_script.sh"
@@ -26,6 +28,10 @@ usage () {
                  Docker registry OAuth2 Server url
               --registry_url
                  Docker registry v2 Server url
+              --from_docker_hub
+                 Get the list of images and tags from Docker Hub
+              --hub_org_name
+                 Images and tags for this organization name (if \`--from_docker_hub\` is set)
               -h | --help
                  Print this help
     "
@@ -56,6 +62,12 @@ until [ -z "$1" ]
        --registry_url)
             _REG_URL=$2
             shift 2;;
+       --from_docker_hub)
+            _USE_DOCKER_HUB=1
+            shift;;
+       --hub_org_name)
+            _HUB_ORG=$2
+            shift 2;;
        -h | --help)
             usage
             exit 1;;
@@ -66,7 +78,7 @@ until [ -z "$1" ]
    esac
   done
 
-if [ -z "${_REG_URL}" ]
+if [[ -z "${_REG_URL}" ]] && [[ "${_USE_DOCKER_HUB}" = "0" ]]
 then
     usage
     exit
@@ -77,11 +89,22 @@ if [[ "${_REG_USERNAME}/" != "/" || "${_REG_PASSWORD}/" != "/" ]]  && [[ "${_REG
     exit 3
 fi
 
+if [[ "${_REG_URL}/" != "/" ]]  && [[ "${_USE_DOCKER_HUB}" == "1" ]]; then
+    echo "[Warning] If you setting --from_docker_hub, registry's url no need to specify."
+    exit 3
+fi
+
+if [[ "${_HUB_ORG}/" == "/" ]]  && [[ "${_USE_DOCKER_HUB}" == "1" ]]; then
+    echo "[Warning] If you setting --from_docker_hub, parameter --hub_org_name also need to specify."
+    exit 3
+fi
+
 if [ "${_REG_BASIC_TOKEN}/" == "/" ]; then
     _REG_BASIC_TOKEN=`echo -n "${_REG_USERNAME}:${_REG_PASSWORD}" | base64`
 fi
 
 echo "[INFO] Generate ${_FILE_NAME} .."
+
 cat > ${_FILE_NAME} <<_FILE_END_
 #/bin/bash
 _REG_BASIC_TOKEN=${_REG_BASIC_TOKEN}
@@ -89,17 +112,18 @@ _REG_AUTH_URL=${_REG_AUTH_URL}
 _REG_URL=${_REG_URL}
 _FILE_END_
 
+DOCKER_REGISTRY_FUNCTION () {
 if [ "${_USE_OAUTH2}" = "1" ] ; then
 cat >> ${_FILE_NAME} <<_FILE_END_
-echo "[INFO] Get Access Token .."
+echo "[INFO] Retrieving access token .."
 _GET_ACCESS_TOKEN=\`curl -s -H "Authorization: Basic \${_REG_BASIC_TOKEN}" "\${_REG_AUTH_URL}/auth?service=Docker+registry&scope=registry:catalog:*"\`
 _ACCESS_TOKEN=\`echo \${_GET_ACCESS_TOKEN} | sed -En 's/.*"access_token":"([^"]*).*/\1/p'\`
-echo "[INFO] Get Catalog .."
+echo "[INFO] Retrieving catalog .."
 _CATELOG=\`curl -s -H "Authorization: Bearer \${_ACCESS_TOKEN}" \${_REG_URL}/v2/_catalog\`
 _FILE_END_
 else
 cat >> ${_FILE_NAME} <<_FILE_END_
-echo "[INFO] Get Catalog .."
+echo "[INFO] Retrieving catalog .."
 _CATELOG=\`curl -s -H "Authorization: Basic \${_REG_BASIC_TOKEN}" \${_REG_URL}/v2/_catalog\`
 _FILE_END_
 fi
@@ -112,7 +136,7 @@ for ((k=0; k<"\${#results_repo_name[@]}"; k++))
 do
     IFS=$'\n' read -r RepoName <<< "\${results_repo_name[\$k]}"
     strRepoName=\`echo \${RepoName} | sed 's/\"//g'\`
-    echo "[INFO] Get Access Token and Tag For Repositories Name: \${strRepoName}"
+    echo "[INFO] Retrieving access token and tag for repository name: \${strRepoName}"
     get_access_token=\`curl -s -H "Authorization: Basic \${_REG_BASIC_TOKEN}" "\${_REG_AUTH_URL}/auth?service=Docker+registry&scope=repository:\${strRepoName}:*"\`
     access_token=\`echo \${get_access_token} | sed -En 's/.*"access_token":"([^"]*).*/\1/p'\`
     curl -s -H "Authorization: Bearer \${access_token}" \${_REG_URL}/v2/\${strRepoName}/tags/list | json_pp
@@ -126,10 +150,51 @@ for ((k=0; k<"\${#results_repo_name[@]}"; k++))
 do
     IFS=$'\n' read -r RepoName <<< "\${results_repo_name[\$k]}"
     strRepoName=\`echo \${RepoName} | sed 's/\"//g'\`
-    echo "[INFO] Get Tag For Repositories Name: \${strRepoName}"
+    echo "[INFO] Retrieving tag for repository name: \${strRepoName}"
     curl -s -H "Authorization: Basic \${_REG_BASIC_TOKEN}" \${_REG_URL}/v2/\${strRepoName}/tags/list | json_pp
 done
 _FILE_END_
+fi
+}
+
+DOCKER_HUB_FUNCTION () {
+cat >> ${_FILE_NAME} <<_FILE_END_
+_TOKEN_ARRAY=( \`echo -n "\${_REG_BASIC_TOKEN}" | base64 --decode | sed -e 's/:/ /g'\` )
+UNAME=\${_TOKEN_ARRAY[0]}
+UPASS=\${_TOKEN_ARRAY[1]}
+HUB_ORG=${_HUB_ORG}
+# get token
+echo "[INFO] Retrieving token ..."
+TOKEN=\$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'\${UNAME}'", "password": "'\${UPASS}'"}' https://hub.docker.com/v2/users/login/ | sed -En 's/.*"token":"([^"]*).*/\1/p' )
+
+# get list of repositories
+echo "[INFO] Retrieving repository list ..."
+REPO_LIST=\$(curl -s -H "Authorization: JWT \${TOKEN}" https://hub.docker.com/v2/repositories/\${HUB_ORG}/?page_size=100 | grep -o '"name":[^,]*' | sed -e 's/"name"://g' | sed -e 's/"//g' )
+
+# output images & tags
+echo
+echo "[INFO] Images and tags for organization: \${HUB_ORG}"
+echo
+for i in \${REPO_LIST}
+do
+  IMAGE_TAGS=\$(curl -s -H "Authorization: JWT \${TOKEN}" https://hub.docker.com/v2/repositories/\${HUB_ORG}/\${i}/tags/?page_size=100 | grep -o '"name":[^,]*' | sed -e 's/"name"://g' | sed -e 's/"//g' )
+
+  JSON=\`echo "{
+      \"name\": \"\${HUB_ORG}/\${i}\",
+      \"tags\": [
+           \$(echo \"\${IMAGE_TAGS}\" | sed -e 's/ /","/g')
+      ]
+  }
+  "\`
+  echo \${JSON} | json_pp
+done
+_FILE_END_
+}
+
+if [ "${_USE_DOCKER_HUB}" = "1" ] ; then
+    DOCKER_HUB_FUNCTION
+else
+    DOCKER_REGISTRY_FUNCTION
 fi
 
 echo "[INFO] Use \`bash ./${_FILE_NAME}\` to enjoy!.."
